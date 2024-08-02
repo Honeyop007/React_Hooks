6e3b63ebf95ad9f76aff85a719989b157c9a7e2b
Abdul Hanan <abdulhana03@gmail.com>
2024-08-03 00:06:45 +0500

    hooks

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,31 +3,30 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+//usestate is a hook that pass a var and a fun in out case we hold var as a count and function as a setcount the set count every time set value of  count
+
+let [count,setcount]= useState(0);
+
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const addval = ()=>{
+   setcount(count++)
+    }
+    const remval = ()=>{
+      setcount(count--)
+      }
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+<h1>Honey Op</h1>
+<h2>Counter value: {count}</h2>
+
+<button onClick={addval}>Enter Value:{count} </button>
+<br /><br />
+<button onClick={remval}>Remove Value:{count} </button>
+<p>The counter is {count} </p>
     </>
   )
 }
